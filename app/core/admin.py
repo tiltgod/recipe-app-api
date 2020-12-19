66e4ba621f384cfe8869b33670abf289e5798852
tiltgod <59070084@it.kmitl.ac.th>
2020-12-20 00:04:36 +0700

    fixing field_sets and add_field_sets

--- app/core/admin.py
+++ app/core/admin.py
@@ -6,23 +6,15 @@ from core import models
 
 
 class UserAdmin(BaseUserAdmin):
+
     ordering = ['id']
     list_display = ['email', 'name']
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ( ('Personal Info'), {'fields': ('name',)}),
-        (
-             ('Permissions'),
-            {'fields': ('is_active','is_staff','is_superuser',)}
-        ),
-        ( ('Important dates'), {'fields': ('last_login',)})
-    )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2')
-        }),
-    )
-
+    fieldsets = ((None, {'fields': ('email', 'password')}),
+                 ('Personal Info', {'fields': ('name', )}),
+                 ('Permissions', {'fields': ('is_active', 'is_staff',
+                 'is_superuser')}), ('Important dates',
+                 {'fields': ('last_login', )}))
+    add_fieldsets = ((None, {'classes': ('wide', ), 'fields': ('email',
+                     'password1', 'password2')}), )
 
 admin.site.register(models.User, UserAdmin)
