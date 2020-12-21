87bb4aed1f2169a784e6f773b1482a080d4e0afd
tiltgod <59070084@it.kmitl.ac.th>
2020-12-21 17:15:04 +0700

    Section 13: Create ingredients endpoint(create ingredient feature)

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,7 +1,6 @@
 """app URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,10 +14,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import user.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include(user.urls)),
+    path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
 ]
